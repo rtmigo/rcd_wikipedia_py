558ede9d2a767a133b690ab3357b10f5458cc524
rtmigo <art.galkin@gmail.com>
2021-07-01 17:11:11 +0300

    lint

--- rcd_wikipedia/__init__.py
+++ rcd_wikipedia/__init__.py
@@ -1,5 +1,2 @@
-from .pedia_to_data import article_url_to_wikidata_id
-
 from .pedia_to_data import articleUrlToWikidataId
-
-# articleUrlToWikidataId
+from .pedia_to_data import article_url_to_wikidata_id
